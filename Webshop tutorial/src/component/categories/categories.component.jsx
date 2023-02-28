0f4d60f091e2d1700bd6245ad665e8a274d729c5
unknown <you@example.com>
2023-02-28 20:12:20 +0100

    section 4

---
+++ Webshop tutorial/src/component/categories/categories.component.jsx
@@ -0,0 +1,14 @@
+import "./categories.style.scss"
+import CategoryItem from "../category-item/category-item.component"
+
+const Categories = ({categories}) => {
+      return (
+        <div className='categories-container'>
+          {categories.map(({ title, id, imageUrl }) => (
+            <CategoryItem  title = {title} id = {id} imageUrl = {imageUrl} />
+          ))}
+        </div>
+      );
+}
+
+export default Categories
